1c25d894c1a3612e3b9aba54a6b959ae4c39b36c
rashidamahroof <rashidamahroofp@gmail.com>
2024-07-14 13:47:07 +0400

    io09o

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ $(document).ready(function(){
         smartSpeed: 450
     });
 });
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
